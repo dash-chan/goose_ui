b38fab40afbb48f62229d489756aaaac01696a30
laiiihz <laiiihz@outlook.com>
2022-01-23 13:49:45 +0800

    add checkbox & checkbox group

--- gallery/lib/home_page.dart
+++ gallery/lib/home_page.dart
@@ -9,6 +9,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GCheckboxController<String> _checkboxController = GCheckboxController({
+    '1': false,
+    '2': null,
+    '3': true,
+  });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,6 +48,49 @@ class _HomePageState extends State<HomePage> {
               child: const Text('null onPressed'),
               icon: const Icon(Icons.ac_unit),
               elevation: 4,
+            ),
+            GRawButton(
+              onPressed: () {},
+              child: const Text('null onPressed'),
+              icon: const Icon(Icons.ac_unit),
+              shape: const StadiumBorder(
+                side: BorderSide(
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            GRawCheckbox(
+              value: null,
+              onChange: (_) {},
+              tristate: true,
+            ),
+            GRawCheckbox(
+              value: null,
+              onChange: (_) {},
+              tristate: true,
+              child: const Text('test checkbox'),
+            ),
+            GCheckboxGroup(
+              controller: _checkboxController,
+              onChange: (item) {
+                print(item);
+              },
+              tristate: true,
+              builder: (BuildContext context, String value) {
+                return Text('checkbox:' + value);
+              },
+            ),
+            GRawButton(
+              onPressed: () {
+                _checkboxController.clear();
+              },
+              child: const Text('clear'),
+            ),
+            GRawButton(
+              onPressed: () {
+                _checkboxController.selectAll();
+              },
+              child: const Text('select All'),
             ),
           ],
         ),
--- packages/goose_ui/lib/goose_ui.dart
+++ packages/goose_ui/lib/goose_ui.dart
@@ -1,3 +1,6 @@
 library goose_ui;
 
 export 'src/widgets/button/g_raw_button.dart';
+
+export 'src/widgets/checkbox/g_raw_checkbox.dart';
+export 'src/widgets/checkbox/g_checkbox_group.dart';
