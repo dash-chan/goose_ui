be232a98d9b652c8c7cdba85c1bfb94a004d33a1
laiiihz <laiiihz@outlook.com>
2022-01-24 14:06:51 +0800

    add radio example

---
+++ gallery/lib/examples/radio_example.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:goose_ui/goose_ui.dart';
+
+class RadioExample extends StatelessWidget {
+  const RadioExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GScaffold(
+      content: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
--- packages/goose_ui/lib/goose_ui.dart
+++ packages/goose_ui/lib/goose_ui.dart
@@ -2,6 +2,8 @@ library goose_ui;
 
 export 'src/widgets/app_bar/g_app_bar.dart';
 
+export 'src/widgets/app_menu/g_app_menu.dart';
+
 export 'src/widgets/drawer/g_drawer.dart';
 
 export 'src/widgets/button/g_raw_button.dart';
@@ -9,6 +11,7 @@ export 'src/widgets/button/g_raw_button.dart';
 export 'src/widgets/checkbox/g_checkbox_group.dart';
 export 'src/widgets/checkbox/g_raw_checkbox.dart';
 
-export 'src/widgets/app_menu/g_app_menu.dart';
+export 'src/widgets/radio/g_raw_radio.dart';
+export 'src/widgets/radio/g_radio_group.dart';
 
 export 'src/widgets/scaffold/g_scaffold.dart';
