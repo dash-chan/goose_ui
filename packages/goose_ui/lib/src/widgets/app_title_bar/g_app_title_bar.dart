ced91049e407b8a4774ac66352a956bbbe6dbc56
laiiihz <laiiihz@outlook.com>
2022-01-26 10:30:33 +0800

    add app title bar

--- packages/goose_ui/lib/src/widgets/app_title_bar/g_app_title_bar.dart
+++ packages/goose_ui/lib/src/widgets/app_title_bar/g_app_title_bar.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:goose_ui/goose_ui.dart';
 
-//TODO
 class GAppTitleBar extends StatelessWidget {
-  const GAppTitleBar({Key? key}) : super(key: key);
+  final Alignment alignment;
+  final VoidCallback? onClose;
+  final VoidCallback? onFullScreen;
+  final VoidCallback? onZoomOut;
+  const GAppTitleBar({
+    Key? key,
+    this.alignment = Alignment.centerLeft,
+    required this.onClose,
+    required this.onFullScreen,
+    required this.onZoomOut,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: alignment,
+      child: Row(
+        children: [
+          GRawButton(
+            height: 24,
+            onPressed: onClose,
+            child: const Icon(Icons.close),
+          ),
+          GRawButton(
+            height: 24,
+            onPressed: onZoomOut,
+            child: const Icon(Icons.remove),
+          ),
+          GRawButton(
+            height: 24,
+            onPressed: onFullScreen,
+            child: const Icon(Icons.fullscreen),
+          ),
+        ],
+      ),
+    );
   }
 }
