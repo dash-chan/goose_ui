da5ecd3571095fce6fd365beab574c929cec9963
laiiihz <laiiihz@outlook.com>
2022-01-26 16:43:36 +0800

    gallery github link

--- gallery/lib/home_page.dart
+++ gallery/lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gallery/pages/main_page.dart';
 import 'package:gallery/pages/widgets_page.dart';
 import 'package:goose_ui/goose_ui.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -35,7 +36,9 @@ class _HomePageState extends State<HomePage> {
             child: const Text('组件'),
           ),
           GRawButton(
-            onPressed: () {},
+            onPressed: () {
+              launch('https://github.com/goose-kit/goose_ui');
+            },
             icon: const Icon(Icons.code_rounded),
             child: const Text('Github'),
           ),
