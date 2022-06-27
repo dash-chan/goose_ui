51d68de3b716f0498bb8056f6f395ec249ecad9a
laiiihz <laiiihz@outlook.com>
2022-06-27 15:11:37 +0800

    switch done

--- apps/goose_gallery/lib/widget_views/switch_view.dart
+++ apps/goose_gallery/lib/widget_views/switch_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goose_gallery/gen/code_segments.dart';
 import 'package:goose_gallery/widgets/playbox.dart';
 import 'package:goose_gallery/widgets/widget_view.dart';
 import 'package:goose_ui/goose_ui.dart';
@@ -8,16 +9,67 @@ class SwitchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const WidgetView(
+    return WidgetView(
       title: 'Switch 开关',
       desc: '',
       children: [
         PlayBox(
-          title: 'default',
-          codes: TextSpan(),
-          desc: 'desc',
-          children: [
+          title: '基本',
+          codes: CodeSegments.switchBase(context),
+          desc: '最简单的用法。',
+          children: const [
             DefaultSwitch(),
+          ],
+        ),
+        PlayBox.single(
+          title: '不可用',
+          codes: CodeSegments.switchDisabled(context),
+          desc: 'Switch 失效状态。',
+          // BEGIN switchDisabled
+          child: const ASwitch(
+            value: true,
+            onChanged: null,
+          ),
+          // END
+        ),
+        PlayBox(
+          title: '文字和图标',
+          codes: CodeSegments.switchText(context),
+          desc: '带有文字和图标。',
+          children: [
+            // BEGIN switchText
+            ASwitch(
+              value: true,
+              checkedItem: const Text('开'),
+              uncheckedItem: const Text('关'),
+              onChanged: (_) {},
+            ),
+            ASwitch(
+              value: true,
+              checkedItem: const Text('1'),
+              uncheckedItem: const Text('0'),
+              onChanged: (_) {},
+            ),
+            // END
+          ],
+        ),
+        PlayBox(
+          title: '两种大小',
+          codes: CodeSegments.switchSize(context),
+          desc: 'size: ASize.small 表示小号开关',
+          children: [
+            // BEGIN switchSize
+            ASwitch(
+              value: true,
+              size: ASize.medium,
+              onChanged: (_) {},
+            ),
+            ASwitch(
+              value: true,
+              size: ASize.small,
+              onChanged: (_) {},
+            ),
+            // END
           ],
         ),
       ],
@@ -33,6 +85,7 @@ class DefaultSwitch extends StatefulWidget {
 }
 
 class _DefaultSwitchState extends State<DefaultSwitch> {
+  // BEGIN switchBase
   bool _state = false;
   @override
   Widget build(BuildContext context) {
@@ -45,4 +98,6 @@ class _DefaultSwitchState extends State<DefaultSwitch> {
       },
     );
   }
+
+  // END
 }
--- packages/goose_ui/lib/src/widgets/switch/switch.dart
+++ packages/goose_ui/lib/src/widgets/switch/switch.dart
@@ -3,7 +3,10 @@ import 'package:goose_ui/goose_ui.dart';
 
 import 'switch_theme.dart';
 
+/// * 需要表示开关状态/两种状态之间的切换时；
+/// * 和 `checkbox` 的区别是，切换 `switch` 会直接触发状态改变，而 `checkbox` 一般用于状态标记，需要和提交操作配合。
 class ASwitch extends StatefulWidget {
+  /// 开关选择器
   const ASwitch({
     super.key,
     required this.value,
@@ -11,18 +14,26 @@ class ASwitch extends StatefulWidget {
     this.checkedItem,
     this.uncheckedItem,
     this.loading = false,
-    this.size,
+    this.size = ASize.medium,
   });
 
+  /// 开关状态 `true` `false`
   final bool value;
+
+  /// 变化时回调函数
   final ValueChanged<bool>? onChanged;
-  // TODO
+
+  /// 选中时的内容
   final Widget? checkedItem;
-  // TODO
+
+  /// 非选中时的内容
   final Widget? uncheckedItem;
+
+  /// 加载中的开关
   final bool loading;
-  // TODO
-  final ASize? size;
+
+  /// 开关大小
+  final ASize size;
 
   @override
   State<ASwitch> createState() => _ASwitchState();
@@ -36,6 +47,7 @@ class _ASwitchState extends State<ASwitch> {
   Color get _trackColor => ASwitchTheme.of(context).computedTrackColor;
   Color get _activeTrackColor =>
       ASwitchTheme.of(context).computedActiveTrackColor;
+
   Widget get _thumb {
     Widget? loadingWidget;
     if (widget.loading) {
@@ -51,15 +63,26 @@ class _ASwitchState extends State<ASwitch> {
     );
   }
 
+  double get _thumbSize {
+    switch (widget.size) {
+      case ASize.small:
+        return 12;
+      case ASize.medium:
+        return 18;
+      case ASize.large:
+        return 18;
+    }
+  }
+
   double get _thumbWidth {
-    return _isTap ? 22 : 18;
+    return _isTap ? _thumbSize + 4 : _thumbSize;
   }
 
   double get _alignLeft {
     return widget.value
         ? _isTap
-            ? 18
-            : 22
+            ? _thumbSize
+            : _thumbSize + 4
         : 0;
   }
 
@@ -69,17 +92,44 @@ class _ASwitchState extends State<ASwitch> {
 
   Widget? get _checkedItem {
     if (widget.checkedItem == null) return null;
-    return DefaultTextStyle(
-      style: const TextStyle(color: Colors.white),
-      child: widget.checkedItem!,
+    Widget result = widget.checkedItem!;
+
+    result = DefaultTextStyle(
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7),
+        child: result,
+      ),
     );
+
+    return result;
+  }
+
+  Widget? get _uncheckedItem {
+    if (widget.uncheckedItem == null) return null;
+    Widget result = widget.uncheckedItem!;
+    result = DefaultTextStyle(
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 12,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 7),
+        child: result,
+      ),
+    );
+
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
     Widget result = SizedBox(
-      width: 44,
-      height: 22,
+      width: (_thumbSize + 4) * 2,
+      height: _thumbSize + 4,
       child: Material(
         shape: _shapeBorder,
         color: widget.value ? _activeTrackColor : _trackColor,
@@ -91,11 +141,25 @@ class _ASwitchState extends State<ASwitch> {
               if (widget.checkedItem != null)
                 AnimatedPositioned(
                   duration: const Duration(milliseconds: 300),
-                  left: widget.value ? 0 : 32,
+                  left: widget.value ? 0 : _thumbSize - 4,
+                  curve: Curves.easeInOutCubic,
                   child: AnimatedOpacity(
                     opacity: widget.value ? 1 : 0,
                     duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOutCubic,
                     child: _checkedItem!,
+                  ),
+                ),
+              if (widget.uncheckedItem != null)
+                AnimatedPositioned(
+                  duration: const Duration(milliseconds: 300),
+                  right: widget.value ? _thumbSize - 4 : 0,
+                  curve: Curves.easeInOutCubic,
+                  child: AnimatedOpacity(
+                    opacity: widget.value ? 0 : 1,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOutCubic,
+                    child: _uncheckedItem!,
                   ),
                 ),
               AnimatedPositioned(
